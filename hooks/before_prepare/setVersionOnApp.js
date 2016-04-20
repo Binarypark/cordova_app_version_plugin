4250885787d97752be4af25e638795ecf024048a
Matthias Tylkowski <matthias@binarypark.org>
2016-04-20 11:23:52 +0200

    switched to q-io, create folder when they don't exist

--- hooks/before_prepare/setVersionOnApp.js
+++ hooks/before_prepare/setVersionOnApp.js
@@ -1,44 +1,69 @@
 #!/usr/bin/env node
-
-
 module.exports = function(context) {
 
-	var fs = context.requireCordovaModule('fs');
-	var path = context.requireCordovaModule('path');
+	var fs = require('q-io/fs');
 	var q = context.requireCordovaModule('q');
-	var writeFile = q.denodeify(fs.writeFile);
 	var semver = context.requireCordovaModule('semver');
 
 	var ConfigParser = context.requireCordovaModule('cordova-lib').configparser;
-	var cfg = new ConfigParser(path.join(context.opts.projectRoot, 'config.xml'));
+	var cfg = new ConfigParser(fs.join(context.opts.projectRoot, 'config.xml'));
 	var version = cfg.version();
 
-	/* after version 5 the plugins are not anymore re-copied into the respective platform folder
+	/**
+	 * after version 5 the plugins are not anymore re-copied into the respective platform folder
 	 * on every build
 	 */
 	if(semver.lt(context.opts.cordova.version, '5.0.0')) {
 
-		return writeFile(
-			context.opts.plugin.dir + '/www/getAppVersion.js',
+		return fs.write(
+			fs.join(context.opts.plugin.dir, '/www/getAppVersion.js'),
 			'module.exports.getAppVersion = function() { return "' + version + '";};'
 		);
 	}
 
+	var pathParts = [
+		'platform_www/plugins',
+		context.opts.plugin.id,
+		'www',
+	];
+
+	var commonPathParts = [
+		context.opts.projectRoot,
+		'platforms',
+	];
+
+	var basePath = fs.join.apply(undefined, commonPathParts);
+	var pluginPath = fs.join.apply(undefined, pathParts);
+
 	function writeWrapper(platformName) {
 
-		return writeFile(
-			path.join(
-				context.opts.projectRoot,
-				'platforms',
-				platformName,
-				'platform_www/plugins',
-				context.opts.plugin.id,
-				'www/getAppVersion.js'
-			),
-			'cordova.define("org.binarypark.cordova.plugins.version.getAppVersion", function(require, exports, module) { module.exports.getAppVersion = function() { return "' + version + '";};});'
+		var fullPath = fs.join(
+			basePath,
+			platformName,
+			pluginPath
 		);
+
+		function writeFile() {
+
+			return fs.write(
+				fs.join(fullPath, 'getAppVersion.js'),
+				'cordova.define("org.binarypark.cordova.plugins.version.getAppVersion", function(require, exports, module) { module.exports.getAppVersion = function() { return "' + version + '";};});'
+			);
+		}
+
+		return fs.exists(fullPath).then(function(pathExists) {
+
+			if(pathExists) {
+				return writeFile();
+			}
+
+			return fs.makeTree(fullPath).then(writeFile);
+		}).catch(function(err) {
+
+				throw(err);
+		});
 	}
-	
+
 	function createWriteCallbacks(platforms) {
 
 		return platforms.map(function(platformName) {
